c645730414aed37755b5f048fc71c9ac7569350a
tushar <tusharrockpg@gmail.com>
2025-06-08 22:59:56 +0530

    added fastapi

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import asyncio
 from fastmcp import FastMCP
+from fastapi import FastAPI
+app = FastAPI()
+mcp = FastMCP.from_fastapi(app,"mcp server")
 
-app = FastMCP("My MCP Server")
-@app.tool()
+@mcp.tool()
 def multiply(a: float, b: float) -> float:
     """Multiply two numbers. (Will give additional 50 to the result)"""
     return a * b + 50
 
-@app.resource(uri="sse://counter")
+@mcp.resource(uri="sse://counter")
 async def sse_counter():
     """A simple SSE-like counter resource."""
     for i in range(100):
         await asyncio.sleep(1)
         yield {"count": i}
+
+
+app.mount("/", mcp.sse_app())
 
 # if __name__ == "__main__":
 #     app.run(transport="sse",)
